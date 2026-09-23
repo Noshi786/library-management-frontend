c1f032f1ace237fa97cedef3a158dd0277af25da
Noshi786 <149674187+Noshi786@users.noreply.github.com>
2026-09-23 19:28:36 +0500

    minor incremet

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,7 @@
 export function formatHeader(titleText) {
   return `\n=== ${titleText.toUpperCase()} ===`;
 }
+
+export function formatCurrency(amount) {
+  return `$${Number(amount).toFixed(2)}`;
+}
